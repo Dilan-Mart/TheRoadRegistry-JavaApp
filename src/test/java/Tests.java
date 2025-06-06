7fe586b8e9f24a489f914434956bef48af79e9a0
VivekReddy14 <crdvrd@gmail.com>
2025-06-06 16:09:05 +1000

    some more comments in test cases file

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -15,17 +15,12 @@ public class Tests {
         File file = new File(FILE_NAME);
         if (file.exists()) file.delete();
     }
-//added comment
-//    @AfterAll
-//    public static void cleanup() {
-//        // Clean up file after tests
-//        File file = new File(FILE_NAME);
-//    }
 
     // ---------------------------
     // Tests for addPerson
     // ---------------------------
 
+    // All valid details test, returns true
     @Test
     public void testAddPerson_Valid() {
       System.out.println("Person added");
@@ -33,24 +28,28 @@ public class Tests {
         assertTrue(p.addPerson());
     }
 
+    // Wrong id returns false
     @Test
     public void testAddPerson_InvalidID() {
         Person p = new Person("12abcdXY", "Dilan", "Fernandez", "10|Main St|Melbourne|Victoria|Australia", "15-11-2000");
         assertFalse(p.addPerson());
     }
 
+    // Wrong address returns false
     @Test
     public void testAddPerson_InvalidAddress() {
         Person p = new Person("78s_d!f#AB", "Varun", "Karthik", "15|Smith St|Sydney|NSW|Australia", "15-11-2000");
         assertFalse(p.addPerson());
     }
 
+    // Wrong DoB returns false
     @Test
     public void testAddPerson_InvalidBirthDate() {
         Person p = new Person("89&^d!x@AB", "Bob", "Lee", "22|Hill St|Melbourne|Victoria|Australia", "1990-11-15");
         assertFalse(p.addPerson());
     }
 
+    // No special characters in ID returns false
     @Test
     public void testAddPerson_InvalidSpecialCharacters() { 
         Person p = new Person("56abcdzAB", "Tom", "Hanks", "1|Star Rd|Melbourne|Victoria|Australia", "15-11-1990");
@@ -61,6 +60,7 @@ public class Tests {
     // Tests for updatePersonalDetails
     // ---------------------------
 
+    // All valid details - returns true
     @Test
     public void testUpdateDetails_AllValid() {
         Person p = new Person("56s_d%&fAB", "John", "Doe", "10|Main St|Melbourne|Victoria|Australia", "15-11-2000");
@@ -69,6 +69,7 @@ public class Tests {
         assertTrue(result);
     }
 
+    // Person under 18 cannot change the address - returns false
     @Test
     public void testUpdateDetails_Under18CannotChangeAddress() {
         Person p = new Person("56s_d%&fAB", "John", "Doe", "5|Main St|Melbourne|Victoria|Australia", "01-01-2010");
@@ -77,6 +78,7 @@ public class Tests {
         assertFalse(result);
     }
 
+    // Updating DoB - returns true
     @Test
     public void testUpdateDetails_BirthdayChangeOnly() {
         Person p = new Person("56s_d%&fAB", "Emma", "Stone", "10|Sunset Blvd|Melbourne|Victoria|Australia", "01-01-1995");
@@ -85,6 +87,7 @@ public class Tests {
         assertTrue(result);
     }
 
+    // Updating multiple with DoB - returns false
     @Test
     public void testUpdateDetails_BirthdayChangeWithOtherChanges() {
         Person p = new Person("56s_d%&fAB", "Leo", "DiCaprio", "11|Sunrise Rd|Melbourne|Victoria|Australia", "01-01-1990");
@@ -93,6 +96,7 @@ public class Tests {
         assertFalse(result);
     }
 
+    // Update doesn't happen if first char is event - returns false
     @Test
     public void testUpdateDetails_IDChangeNotAllowedIfFirstCharEven() {
         Person p = new Person("62a_d!x@AB", "Eve", "Black", "5|St Kilda Rd|Melbourne|Victoria|Australia", "01-01-1990");
@@ -105,6 +109,7 @@ public class Tests {
     // Tests for addDemeritPoints
     // ---------------------------
 
+    // Adds demerit points but account not suspended because points < 12
     @Test
     public void testAddDemeritPoints_ValidCase() {
         Person p = new Person("78a_x!z@AB", "Clark", "Kent", "2|Hero Ln|Melbourne|Victoria|Australia", "01-01-2000");
@@ -114,6 +119,7 @@ public class Tests {
         assertFalse(p.isSuspended());
     }
 
+    // Account suspends for under 21 and demerit points > 6
     @Test
     public void testAddDemeritPoints_ExceedsThreshold_Under21() {
         Person p = new Person("79b_d!z@AB", "Peter", "Parker", "3|Web St|Melbourne|Victoria|Australia", "01-01-2007");
@@ -123,6 +129,7 @@ public class Tests {
         assertTrue(p.isSuspended());
     }
 
+    // Account suspends for over 21 and demerit points > 12
     @Test
     public void testAddDemeritPoints_ExceedsThreshold_Over21() {
         Person p = new Person("89x_c@!dAB", "Bruce", "Wayne", "100|Batcave Rd|Melbourne|Victoria|Australia", "01-01-1990");
@@ -135,6 +142,7 @@ public class Tests {
         assertTrue(p.isSuspended());
     }
 
+    // Invalid date, demerit points not added
     @Test
     public void testAddDemeritPoints_InvalidDate() {
         Person p = new Person("93m_d!z@AB", "Tony", "Stark", "108|Arc Rd|Melbourne|Victoria|Australia", "01-01-1985");
@@ -143,6 +151,7 @@ public class Tests {
         assertEquals("Failed", result);
     }
 
+    // Invalid number of points, > 6
     @Test
     public void testAddDemeritPoints_InvalidPoints() {
         Person p = new Person("95n_d!z@AB", "Natasha", "Romanoff", "9|Spy St|Melbourne|Victoria|Australia", "01-01-1992");
