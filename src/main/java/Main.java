1ecaea7fb1eabb958d8c9f386eae0e3c6c142836
dilan martin fernandez <mdilan4518@gmail.com>
2025-06-06 18:38:19 +1000

    fix bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 //    	testing
-        Person p = new Person("12b_d!z@AB", "Peter", "Parker","3|Web St|Melbourne|Victoria|Australia","01-11-2012");
+        Person p = new Person("29s_d%&fAB", "Peter", "Parker","3|Web St|Melbourne|Victoria|Australia","01-11-2012");
+        p.addPerson();
     }
 }
